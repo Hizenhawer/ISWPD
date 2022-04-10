cdeb68c7ababeffd3e507416d0a8422f91ec8558
Wiciu <witoldkesek@gmail.com>
2022-04-10 13:38:17 +0200

    added the Route class

--- DatabaseHandler.py
+++ DatabaseHandler.py
@@ -8,5 +8,19 @@ class Color(Enum):
     GREEN = 4
 
 
+class Route:
+    def __init__(self, label, start_point, end_point, color, elevation, descent, length, time):
+        self.label = label
+        self.start_point = start_point
+        self.end_point = end_point
+        self.color = color,
+        self.elevation = elevation
+        self.descent = descent,
+        self.length = length,
+        self.time = time
+
+
 data = [
-    ["Schronisko PTTK Na Hali Miziowej ↔ Las Suchowarski", (49.540217, 19.318807), (49.534830, 19.343204), Color.RED, 40.0, 168.0, 2.3, (40, 50)]]
+    Route("Schronisko PTTK Na Hali Miziowej ↔ Las Suchowarski", (49.540217, 19.318807), (49.534830, 19.343204), Color.RED, 40.0, 168.0, 2.3, (40, 50)),
+    Route("Odbočka Zimnej cesty na Štrbské Pleso ↔ Trigan", (49.151894, 20.076688), (49.136647, 20.067202), Color.GREEN, 115.0, 99.0, 2.0, (40, 50)),
+        ]
