fa3b9c2c917c757273819da6219dd52c8c5cec73
Hizenhawer <tomek.herzyk@gmail.com>
2022-04-10 12:56:04 +0200

    adsasd

---
+++ DatabaseHandler.py
@@ -0,0 +1,8 @@
+from enum import Enum
+class Color(Enum):
+    RED = 1
+    BLUE = 2
+    BLACK = 3
+    GREEN = 4
+
+data = [["Schronisko PTTK Na Hali Miziowej ↔ Las Suchowarski",(49.540217,19.318807),(49.534830,19.343204),Color.RED,]]
