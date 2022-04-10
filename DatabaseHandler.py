d20613a9a19aac7b2a9255a4bdca458e60648976
Hizenhawer <tomek.herzyk@gmail.com>
2022-04-10 13:04:08 +0200

    adsasd

--- DatabaseHandler.py
+++ DatabaseHandler.py
@@ -1,8 +1,12 @@
 from enum import Enum
+
+
 class Color(Enum):
     RED = 1
     BLUE = 2
     BLACK = 3
     GREEN = 4
 
-data = [["Schronisko PTTK Na Hali Miziowej ↔ Las Suchowarski",(49.540217,19.318807),(49.534830,19.343204),Color.RED,]]
+
+data = [
+    ["Schronisko PTTK Na Hali Miziowej ↔ Las Suchowarski", (49.540217, 19.318807), (49.534830, 19.343204), Color.RED, 40.0, 168.0, 2.3, (40, 50)]]
